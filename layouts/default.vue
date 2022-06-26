7dcd11698a8ffa67fd828930f52142eb4d213c8c
kiss8981 <kdh89811@gmail.com>
2022-06-26 17:20:05 +0900

    fix: github link

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
                 .searchArea(class="w-4/6")
                     search
                 .iconArea
-                    a(href="https://github.com/archive-discord/archvie-docs")
+                    a(href="https://github.com/archive-discord/archive-docs")
                         i.fab.fa-github
         
         .h-16
