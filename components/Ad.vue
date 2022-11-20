403122cd660c5b09d850c563a04ebd15720553a6
kiss8981 <kdh89811@gmail.com>
2022-11-20 19:36:52 +0900

    fix: ads

--- components/Ad.vue
+++ components/Ad.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <ins class="adsbygoogle pt-4"
-     style="display:inline-block;width:100%;height:90px"
-     data-ad-client="ca-pub-2701426579223876"
-     data-ad-slot="8854969692">
-     </ins>
+    <ins
+      class="adsbygoogle pt-4"
+      style="display: inline-block; width: 100%; height: 90px"
+      data-ad-client="ca-pub-2701426579223876"
+      data-ad-slot="8854969692"
+    >
+    </ins>
   </div>
 </template>
 
 <script>
 export default {
-  mounted(){
-    try{
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    } catch(e) {
-      console.log(e, "Failed Adsense")
+  mounted() {
+    try {
+      var isAdsenseNotLoaded =
+        !adsbygoogle || typeof adsbygoogle.loaded === "undefined";
+      if (isAdsenseNotLoaded) {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (e) {
+      console.log(e, "Failed Adsense");
     }
-  }
-}
+  },
+};
 </script>
